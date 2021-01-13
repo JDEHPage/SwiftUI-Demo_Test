98fc7b641d161354838bf4ed6b470fecd38f5df3
John_D.Page <jdehpage.w@gmail.com>
2021-01-13 19:56:44 +0000

    Added Map View

---
+++ SwiftUI Demo_Test/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  SwiftUI Demo_Test
+//
+//  Created by John Daniel Page on 13/01/2021.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -23.923_32, longitude: 31.267_9), span: MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015)
+    )
+    
+//    -23.9231644,31.26791
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+    
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
